96db6368e26c1945c5d900fa19ff3e1b011b6c48
Reza Rastégar <reza.rastegar@gmx.de>
2025-01-18 14:07:42 +0100

    Groovy test complete

--- nifi-cdp-processors/src/test/java/org/rra/processors/GroovyTest.java
+++ nifi-cdp-processors/src/test/java/org/rra/processors/GroovyTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 public class GroovyTest {
     @Test
     public void testGroovy(){
+
         ExecuteGroovyScript executeGroovyScript = new ExecuteGroovyScript();
     }
 }
---
+++ nifi-cdp-processors/src/test/java/org/rra/processors/TestGroovyHL7.java
@@ -0,0 +1,35 @@
+package org.rra.processors;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestGroovyHL7 {
+
+    @BeforeEach
+    public void setup() throws Exception {
+
+        //init processor
+//        proc = new ExecuteGroovyScript();
+//        MockProcessContext context = new MockProcessContext(proc);
+//        MockProcessorInitializationContext initContext = new MockProcessorInitializationContext(proc, context);
+//        proc.initialize(initContext);
+//
+//        assertNotNull(proc.getSupportedPropertyDescriptors());
+//        runner = TestRunners.newTestRunner(proc);
+//        runner.addControllerService("dbcp", dbcp, new HashMap<>());
+//        runner.enableControllerService(dbcp);
+//
+//        List<RecordField> recordFields = Arrays.asList(
+//                new RecordField("id", RecordFieldType.INT.getDataType()),
+//                new RecordField("name", RecordFieldType.STRING.getDataType()),
+//                new RecordField("code", RecordFieldType.INT.getDataType()));
+//
+//        recordParser = new MockRecordParser();
+//        recordFields.forEach((r) -> recordParser.addSchemaField(r));
+//        runner.addControllerService("myreader", recordParser, new HashMap<>());
+//        runner.enableControllerService(recordParser);
+//
+//        recordWriter = new MockRecordWriter();
+//        runner.addControllerService("mywriter", recordWriter, new HashMap<>());
+//        runner.enableControllerService(recordWriter);
+    }
+}
