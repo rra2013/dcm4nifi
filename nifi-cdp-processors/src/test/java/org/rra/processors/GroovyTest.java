372cae8e6a6103ec5452e1fb036b5f954e505fe6
dev <reza.rastegar@insel.ch>
2025-01-18 12:12:03 +0100

    maven groovy test

---
+++ nifi-cdp-processors/src/test/java/org/rra/processors/GroovyTest.java
@@ -0,0 +1,11 @@
+package org.rra.processors;
+
+import org.apache.nifi.processors.groovyx.ExecuteGroovyScript;
+import org.junit.jupiter.api.Test;
+
+public class GroovyTest {
+    @Test
+    public void testGroovy(){
+        ExecuteGroovyScript executeGroovyScript = new ExecuteGroovyScript();
+    }
+}
